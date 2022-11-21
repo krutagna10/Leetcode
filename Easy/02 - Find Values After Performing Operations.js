864ff4a5e6952a216ef95dc13ec4446d44b0a397
Krutagna Patel <kpatel.kp7016@gmail.com>
2022-11-21 16:11:16 +0530

    .

--- Easy/02 - Find Values After Performing Operations.js
+++ Easy/02 - Find Values After Performing Operations.js
@@ -3,11 +3,7 @@
 const finalValueAfterOperations = (operations) => {
     let x = 0;
     for (const operator of operations) {
-        if (operator === 'X++' || operator === '++X') {
-            x = x + 1;
-        } else {
-            x = x - 1;
-        }
+        operator[1] === '+' ? x = x + 1 : x = x - 1;
     }
     return x;
 }
--- Easy/03 - Concatenation Of Array.js
+++ Easy/03 - Concatenation Of Array.js
@@ -1,2 +1,19 @@
 'use strict';
 
+const getConcatenation = (arr) => {
+   const ans = [];
+   for (let i = 0; i < arr.length; i++) {
+       ans[i] = arr[i];
+       ans[arr.length + i] = arr[i];
+   }
+   return ans;
+}
+
+// Second Method
+const getConcatenation1 = (arr) => {
+    return [...arr, ...arr];
+}
+
+const arr = [1, 2, 3];
+console.log(getConcatenation(arr));
+
