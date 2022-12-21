9cf12556e639a3af740dd1229c18ab3ab36960cf
Krutagna <kpatel.kp7016@gmail.com>
2022-12-21 21:29:41 +0530

    .

--- 01 - Easy/25 - Happy Number.js
+++ 01 - Easy/25 - Happy Number.js
@@ -1,5 +1,26 @@
 'use strict';
 
 const isHappy = (n) => {
-    
-}
+    let set = new Set();
+    while (!set.has(n)) {
+        let sum = 0;
+        set.add(n);
+
+        while (n > 0) {
+            let remainder = n % 10;
+            sum = sum + remainder * remainder;
+            n = Math.floor(n / 10);
+        }
+
+        if (sum === 1) {
+            return true;
+        } else {
+            n = sum;
+        }
+    }
+    return false;
+};
+
+const n = 19;
+console.log(isHappy(n));
+
