f61e4e6d562d2ac999faccaec9e02e2b7512d6cf
Krutagna <kpatel.kp7016@gmail.com>
2022-12-24 08:34:40 +0530

    .

--- 02 - Medium/05 - Search in Rotated Sorted Array.js
+++ 02 - Medium/05 - Search in Rotated Sorted Array.js
@@ -1,5 +1,54 @@
 'use strict';
 
-const search = (nums, target) => {
-
+const search = (arr, target) => {
+    const pivot = findPivot(arr);
+    if (pivot === -1) {
+        return binarySearch(arr, target, 0, arr.length -1);
+    }
+    if (target === arr[pivot]) {
+        return pivot;
+    }
+    if (target >= arr[0]) {
+        return binarySearch(arr, target, 0, pivot - 1);
+    } else {
+        return binarySearch(arr, target, pivot + 1, arr.length - 1);
+    }
 };
+
+const findPivot = (arr) => {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while(start <= end) {
+        let mid = start + Math.floor((end - start) / 2);
+        if (mid < end && arr[mid] > arr[mid + 1]) {
+            return mid;
+        }
+        if (mid > start && arr[mid] < arr[mid - 1]) {
+            return mid - 1;
+        }
+        if (arr[start] > arr[mid]) {
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return -1;
+};
+
+const binarySearch = (arr, target, start, end) => {
+    while (start <= end) {
+        let mid = start + Math.floor((end - start) / 2);
+        if (target === arr[mid]) {
+            return mid;
+        } else if (target > arr[mid]) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return -1;
+};
+
+const arr = [3, 4, 1, 2];
+console.log(search(arr, 3));
