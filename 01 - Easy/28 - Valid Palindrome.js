c542a236e2660ca90b73e2af4879b3cc172b4d7f
Krutagna <kpatel.kp7016@gmail.com>
2022-12-24 18:26:19 +0530

    .

---
+++ 01 - Easy/28 - Valid Palindrome.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const isPalindrome = (str) => {
+    str = str.toLowerCase().replaceAll(/[^a-z0-9]/gi, '');
+    let originalStr = str;
+    let reverseStr = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr = reverseStr + str.charAt(i);
+    }
+    return originalStr === reverseStr;
+};
+
+const isPalindrome1 = (str) => {
+    str = str.toLowerCase().replaceAll(/[^a-z0-9]/gi, '');
+    return str === str.split('').reverse().join('')
+};
+
+const str = 'A man, a plan, a canal: Panama';
+console.log(isPalindrome(str));
