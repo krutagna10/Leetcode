17be64cf010de07a17def69dd9d6516de2da4eab
Krutagna <kpatel.kp7016@gmail.com>
2022-12-24 08:15:24 +0530

    .

---
+++ 01 - Easy/26 - Longest Common Prefix.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const longestCommonPrefix = (str) => {
+    if (str.length === 0) {
+        return '';
+    }
+    let currentStr = str[0];
+    let tempStr = '';
+    for (let i = 1; i < str.length; i++) {
+        for (let j = 0; j < str[i].length; j++) {
+            if (currentStr[j] === str[i][j]) {
+                tempStr = tempStr + currentStr[j];
+            } else {
+                break;
+            }
+        }
+        currentStr = tempStr;
+        tempStr = '';
+    }
+    return currentStr;
+};
+
+const longestCommonPrefix1 = (str) => {
+    if (str.length === 0) {
+        return '';
+    }
+
+    return str.reduce((acc, element) => {
+        let i = 0;
+        while (acc[i] && element[i] && acc[i] === element[i]) {
+            i++;
+        }
+        return acc.slice(0, i);
+    });
+};
+
+const str = ['flower', 'flow', 'flight'];
+console.log(longestCommonPrefix1(str))
+
+console.log(3 && 0)
