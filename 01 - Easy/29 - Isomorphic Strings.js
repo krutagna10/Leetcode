396fa45886bac6111f2ff449f9fe9d61e0a208c0
Krutagna <kpatel.kp7016@gmail.com>
2022-12-25 12:22:01 +0530

    .

--- 01 - Easy/29 - Isomorphic Strings.js
+++ 01 - Easy/29 - Isomorphic Strings.js
@@ -3,8 +3,6 @@
 const isIsomorphic = (str1, str2) => {
     const arr1 = [];
     const arr2 = [];
-    let k = 0;
-    let ans = true;
 
     // Initial condition
     if (str1.length !== str2.length) {
@@ -15,16 +13,14 @@ const isIsomorphic = (str1, str2) => {
         let ch1 = str1.charAt(i);
         let ch2 = str2.charAt(i);
 
-        if (arr1[ch1] === arr2[ch2]) {
-            arr1[ch1] = k;
-            arr2[ch2] = k;
-            k++;
+        if (arr1[ch1] !== arr2[ch2]) {
+            return false;
         } else {
-            ans = false;
-            break;
+            arr1[ch1] = i + 1;
+            arr2[ch2] = i + 1;
         }
     }
-    return ans;
+    return true;
 };
 
 const str1 = 'aaa';
