c000f140d10743e0bcd8474e6c8e918edc3ebd74
Krutagna <kpatel.kp7016@gmail.com>
2022-12-27 12:17:47 +0530

    .

--- 01 - Easy/33 - Intersection of Two Arrays II.js
+++ 01 - Easy/33 - Intersection of Two Arrays II.js
@@ -1,16 +1,22 @@
 'use strict';
 
 const intersect = (arr1, arr2) => {
+    // Sorting the arrays
+    arr1.sort((a, b) => a - b);
+    arr2.sort((a, b) => a - b);
+
     let i = 0;
     let j = 0;
     const ans = [];
     while (i < arr1.length && j < arr2.length) {
         if (arr1[i] === arr2[j]) {
             ans.push(arr1[i]);
-        } else if (arr1[i] > arr2[j]) {
+            i++;
+            j++;
+        } else if (arr1[i] < arr2[j]) {
             i++;
         } else {
-            j++;
+            j++
         }
     }
     return ans.length === 0 ? [-1, -1] : ans;
@@ -19,3 +25,5 @@ const intersect = (arr1, arr2) => {
 const arr1 = [1, 2, 2, 1];
 const arr2 = [2, 2];
 console.log(intersect(arr1, arr2));
+
+c
