526a9d92bc05116cccfcc62b47e9fb1117b3366e
Krutagna <kpatel.kp7016@gmail.com>
2022-12-29 09:58:55 +0530

    .

--- 01 - Easy/39 - Next Greater Element 1.js
+++ 01 - Easy/39 - Next Greater Element 1.js
@@ -1,5 +1,29 @@
 'use strict';
 
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(element) {
+        return this.items.push(element);
+    }
+
+    pop() {
+        if (this.items.length > 0) {
+            return this.items.pop();
+        }
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
+
 const nextGreaterElement = (arr1, arr2) => {
     const elementArr = createElementArr(arr2);
     const map = new Map();
@@ -16,24 +40,28 @@ const nextGreaterElement = (arr1, arr2) => {
 };
 
 const createElementArr = (arr) => {
-    const elementArr = [];
+    let ans = [];
+    let stack = new Stack();
+    stack.push(0);
 
-    for (let i = 0; i < arr.length - 1; i++) {
-        let flag = false;
-        for (let j = i + 1; j < arr.length; j++) {
-            if (arr[j] > arr[i]) {
-                elementArr.push(arr[j]);
-                flag = true;
-                break;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < arr[stack.peek()]) {
+            stack.push(i);
+        } else {
+            while (arr[i] > arr[stack.peek()]) {
+                ans[stack.peek()] = arr[i];
+                stack.pop();
             }
-        }
-        if (!flag) {
-            elementArr.push(-1);
+            stack.push(i);
         }
     }
-    elementArr.push(-1);
 
-    return elementArr;
+//    Putting -1 in remaining elements
+    while (!stack.isEmpty()) {
+        ans[stack.peek()] = -1;
+        stack.pop();
+    }
+    return ans;
 }
 
 const arr = [4, 1, 2];
