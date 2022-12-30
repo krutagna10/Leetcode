072dcd42390542d7e1e93f3e880b6b12682f03eb
Krutagna <kpatel.kp7016@gmail.com>
2022-12-30 09:55:52 +0530

    .

---
+++ 01 - Easy/44 - Check if the Sentence is Pangram.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const checkIfPangram = (sentence) => {
+    for (let i = 97; i <= 122; i++) {
+        let ch = String.fromCharCode(i);
+        if (!sentence.includes(ch)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const checkIfPangram1 = (sentence) => {
+    return new Set(sentence).size === 26;
+}
+
+const sentence = 'thequickbrownfoxjumpsoverthelazydog';
+console.log(checkIfPangram(sentence));
+
+
+
