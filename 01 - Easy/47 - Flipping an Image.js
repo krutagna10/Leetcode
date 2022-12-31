fe4c48037b5d1291391c2ed819008c494d6cedac
Krutagna <kpatel.kp7016@gmail.com>
2022-12-31 12:38:47 +0530

    .

---
+++ 01 - Easy/47 - Flipping an Image.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const flipAndInvertImage = (image) => {
+
+};
+
+const image = [[1,1,0],
+    [1,0,1],
+    [0,0,0]];
+console.log(flipAndInvertImage(image))
---
+++ 02 - Medium/10 - Rotate The Array.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const rotateArray = (arr, k) => {
+    k = k % arr.length;
+    reverse(arr, 0, arr.length - 1);
+    reverse(arr, 0, k - 1);
+    reverse(arr, k, arr.length - 1);
+}
+
+const reverse = (arr, start, end) => {
+    while (start < end) {
+        let temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+const k = 3;
+rotateArray(arr, k);
+console.log(arr);
