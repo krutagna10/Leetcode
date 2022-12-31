ae1719d6575a232709adfb4ec72fe6df8bfb22be
Krutagna <kpatel.kp7016@gmail.com>
2022-12-31 11:31:15 +0530

    .

--- 01 - Easy/45 -Find The Highest Altitude.js
+++ 01 - Easy/45 -Find The Highest Altitude.js
@@ -1,8 +1,16 @@
 'use strict';
 
 const largestAltitude = (gain) => {
-
+    let maxAltitude = 0;
+    let altitude = 0;
+    for (let i = 0; i < gain.length; i++) {
+        altitude = gain[i] + altitude;
+        if (altitude > maxAltitude) {
+            maxAltitude = altitude;
+        }
+    }
+    return maxAltitude;
 };
 
 const gain = [-5 , 1, 5, 0, -7];
-// -4,-3,-2,-1,4,3,2 -> 0
+console.log(largestAltitude(gain));
