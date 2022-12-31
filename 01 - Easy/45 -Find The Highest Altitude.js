4cb4f733b9eb3445fd230631422465b32d63d93e
Krutagna <kpatel.kp7016@gmail.com>
2022-12-31 11:20:29 +0530

    .

---
+++ 01 - Easy/45 -Find The Highest Altitude.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const largestAltitude = (gain) => {
+
+};
+
+const gain = [-5 , 1, 5, 0, -7];
+// -4,-3,-2,-1,4,3,2 -> 0
--- 02 - Medium/09 - Furthest Building You Can Reach.js
+++ 02 - Medium/09 - Furthest Building You Can Reach.js
@@ -1,9 +1,109 @@
 'use strict';
 
-const furthestBuilding = (heights, bricks, ladders) => {
+class Heap {
+    constructor() {
+        this.values = [];
+    }
 
+    getParentIndex(index) {
+        return Math.floor((index - 1) / 2);
+    }
+
+    getLeftChildIndex(index) {
+        return index * 2 + 1;
+    }
+
+    getRightChildIndex(index) {
+        return index * 2 + 2;
+    }
+
+    swap(index1, index2) {
+        let temp = this.values[index1];
+        this.values[index1] = this.values[index2];
+        this.values[index2] = temp;
+    }
+
+    heapifyUp(index) {
+        let currentIndex = index;
+        let parentIndex = this.getParentIndex(index);
+
+        while (currentIndex > 0 && this.values[currentIndex] > this.values[parentIndex]) {
+            this.swap(currentIndex, parentIndex);
+            currentIndex = parentIndex;
+            parentIndex = this.getParentIndex(currentIndex);
+        }
+    }
+
+    heapifyDown() {
+        let currentIndex = 0;
+
+        while(this.values[this.getLeftChildIndex(currentIndex)] !== undefined) {
+            let biggestChildIndex = this.getLeftChildIndex(currentIndex);
+
+            if (this.values[this.getRightChildIndex(currentIndex)] !== undefined && this.values[this.getRightChildIndex(currentIndex)] > this.values[this.getLeftChildIndex(currentIndex)] ) {
+                biggestChildIndex = this.getRightChildIndex(currentIndex);
+            }
+
+            if (this.values[currentIndex] < this.values[biggestChildIndex]) {
+                this.swap(currentIndex, biggestChildIndex);
+                currentIndex = biggestChildIndex;
+            } else {
+                return;
+            }
+        }
+    }
+
+    add(element) {
+        this.values.push(element);
+        this.heapifyUp(this.values.length - 1);
+    }
+
+    peek() {
+        return this.values[0];
+    }
+
+    poll() {
+        const maxValue = this.values[0];
+        this.values[0] = this.values[this.values.length - 1];
+        this.values.length = this.values.length - 1;
+        this.heapifyDown();
+        return maxValue;
+    }
+}
+
+
+
+const furthestBuilding = (heights, bricks, ladders) => {
+    let heap = new Heap();
+
+    for(let i = 0; i < heights.length - 1; i++) {
+        if (heights[i] >= heights[i + 1]) {
+            continue;
+        }
+        let jump = heights[i + 1] - heights[i];
+        bricks = bricks - jump;
+        heap.add(jump);
+        if (bricks < 0) {
+            console.log(heap);
+            bricks = bricks + heap.poll();
+            console.log(bricks);
+            if (ladders > 0) {
+                ladders--;
+            }
+            else {
+                return i;
+            }
+        }
+
+    }
+    return heights.length - 1;
 };
+
+
 
 const heights = [4, 2, 7, 6, 9, 14, 12];
 const bricks = 5;
 const ladders = 1;
+
+console.log(furthestBuilding(heights, bricks, ladders))
+
