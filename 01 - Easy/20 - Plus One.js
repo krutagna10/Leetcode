f74fcb0c95ca2f485d3f12c8afd983b4b060512e
Krutagna <kpatel.kp7016@gmail.com>
2023-01-04 19:17:25 +0530

    .

--- 01 - Easy/20 - Plus One.js
+++ 01 - Easy/20 - Plus One.js
@@ -5,6 +5,19 @@ const plusOne = (digits) => {
     return num.toString().split('');
 };
 
+const plusOne1 = (digits) => {
+    let ans = [];
+    let index = 0;
+    for (let i = digits.length - 1; i >= 0; i--) {
+        if (digits[i] < 9) {
+            digits[i] = digits[i] + 1;
+            return digits;
+        } else {
+            digits[i] = 0;
+        }
+    }
+}
+
 const digits = [1, 2, 4];
-console.log(plusOne(digits));
+console.log(plusOne1(digits));
 
