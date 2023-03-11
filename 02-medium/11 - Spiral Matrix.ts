20c6c96c5a7303fbc72920df3bfb39bb631e5917
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-11 12:41:52 +0530

    spiral matrix question added

---
+++ 02-medium/11 - Spiral Matrix.ts
@@ -0,0 +1,51 @@
+const spiralMatrix = (matrix: number[][]): number[] => {
+  const result = [];
+
+  let rowBegin = 0;
+  let rowEnd = matrix.length - 1;
+
+  let columnBegin = 0;
+  let columnEnd = matrix[0].length - 1;
+
+  while (rowBegin <= rowEnd && columnBegin <= columnEnd) {
+    // Traverse the top row
+    for (let i = columnBegin; i <= columnEnd; i++) {
+      result.push(matrix[rowBegin][i]);
+    }
+
+    rowBegin++;
+
+    // Traverse the right column
+    for (let i = rowBegin; i <= rowEnd; i++) {
+      result.push(matrix[i][columnEnd]);
+    }
+
+    columnEnd--;
+
+    // Traverse the bottom row (if it exits)
+    if (rowBegin <= rowEnd) {
+      for (let i = columnEnd; i >= 0; i--) {
+        result.push(matrix[rowEnd][i]);
+      }
+      rowEnd--;
+    }
+
+    // Traverse the left column (if it exists)
+    if (columnBegin <= columnEnd) {
+      for (let i = rowEnd; i >= rowBegin; i--) {
+        result.push(matrix[i][columnBegin]);
+      }
+      columnBegin++;
+    }
+  }
+
+  return result;
+};
+
+const matrix: number[][] = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+];
+
+console.log(spiralMatrix(matrix));
--- README.md
+++ README.md
@@ -5,4 +5,4 @@ LeetCode is the best platform to help you enhance your skills, expand your knowl
 - Day 1: March 8, 2023
 - Day 2: March 9, 2023
 - Day 3: March, 10, 2023
-
+- Day 4: March 11, 2023
