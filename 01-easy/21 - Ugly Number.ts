606b9f061b772cb70bad6957d782ce3e58d1b0a9
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-03-14 10:37:15 +0530

    .

---
+++ 01-easy/21 - Ugly Number.ts
@@ -0,0 +1,19 @@
+const isUgly = (n: number): boolean => {
+  if (n === 0) {
+    return false;
+  }
+
+  while (n % 5 === 0) {
+    n = n / 5;
+  }
+  while (n % 3 === 0) {
+    n = n / 3;
+  }
+  while (n % 2 === 0) {
+    n = n / 2;
+  }
+  return n === 1;
+};
+
+const n: number = 6;
+console.log(isUgly(n));
